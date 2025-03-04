4e596f104d209a7a7ce0c4b72ffd946f0c404c7c
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2025-03-04 18:25:56 +0100

    Unused usings (que no imorts ja ja...)

--- XCVTransformer/MainWindow.xaml.cs
+++ XCVTransformer/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
-
-using XCVTransformer.Helpers;
-using XCVTransformer.WinSubclasses;
-
-using System.Runtime.InteropServices;
 using WinRT.Interop;
 using XCVTransformer.AuxClasses;
 
