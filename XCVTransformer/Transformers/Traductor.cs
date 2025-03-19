dbb8afcd833909dbdde250ed2a64ae54a3254be5
Pedro <uo288120@uniovi.es>
2025-03-19 19:41:04 +0100

    Comentado y refactorizado la clase Traductor

--- XCVTransformer/Transformers/Traductor.cs
+++ XCVTransformer/Transformers/Traductor.cs
@@ -9,29 +9,39 @@ namespace XCVTransformer.Transformers
 {
     class Traductor : ITransformer
     {
+        private string fromLanguage = "es";
+        private string toLanguage = "en";
+
         private string apiKey = Environment.GetEnvironmentVariable("MY_TRANSLATOR_API_KEY");
         private static readonly string endpoint = "https://api.cognitive.microsofttranslator.com/";
-        private static readonly string route = "/translate?api-version=3.0&to=en&from=es"; // Traducción a inglés
 
+        /**
+         * Método que llama a la API de mi recurso traductor de Azure, depende de los atributos to y from los idiomas a destino y origen.
+         * 
+         * Método POST
+         * En la cabezera va la API key y la región
+         * En el body va solamente el texto a transformar como Text
+         * Se envía y recibe la respuesta en formato JSON
+         * 
+         * Se devuelve el texto traducido
+         */
         public async Task<string> Transform(string toTransform)
-        {      
+        {
+            var route = $"/translate?api-version=3.0&from={fromLanguage}&to={toLanguage}";
+
             using (var client = new HttpClient())
             {
-                // Configurar la solicitud HTTP
                 client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
-                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Region", "westeurope"); // Cambia la región si es necesario
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Region", "westeurope");
 
                 var requestBody = new[] { new { Text = toTransform } };
                 var content = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
 
-                // Hacer la solicitud POST a la API de Microsoft Translator
                 var response = await client.PostAsync(endpoint + route, content);
                 response.EnsureSuccessStatusCode();
 
-                // Leer la respuesta JSON
                 var result = await response.Content.ReadAsStringAsync();
 
-                // Procesar el resultado de la traducción
                 var translatedText = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(result)[0].translations[0].text;
                 return translatedText;
             }
